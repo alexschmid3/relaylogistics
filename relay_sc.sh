245b28e68a14249040ddb4cdbdfff391ca88a059
Alex Schmid <alexandriaschmid3@gmail.com>
2024-01-30 12:37:20 -0500

    Add batch file for SuperCloud

---
+++ relay_sc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -a 1449-1450
+#SBATCH --partition=xeon-p8
+#SBATCH --time=2-00:00
+#SBATCH -o /home/gridsan/aschmid/relaylogistics/outerr/resub_%a.out
+#SBATCH -e /home/gridsan/aschmid/relaylogistics/outerr/resub_%a.err
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=aschmid@mit.edu
+
+module load julia/1.9.2
+module load gurobi/gurobi-1000
+
+julia run_static.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
