5cfc1af10616cd0f004f1116fc3b93b801620d3b
Alex Schmid <alexandriaschmid3@gmail.com>
2024-04-14 21:03:10 -0400

    Update SuperCloud batch file

--- relay_sc.sh
+++ relay_sc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -a 11-24
+#SBATCH -a 1089-1100
 #SBATCH --partition=xeon-p8
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=64G
-#SBATCH --time=2-00:00
-#SBATCH -o /home/gridsan/aschmid/relaylogistics/outerr/table3_%a.out
-#SBATCH -e /home/gridsan/aschmid/relaylogistics/outerr/table3_%a.err
+#SBATCH --mem=32G
+#SBATCH --time=1-04:00
+#SBATCH -o /home/gridsan/aschmid/relaylogistics/outerr/table2_%a.out
+#SBATCH -e /home/gridsan/aschmid/relaylogistics/outerr/table2_%a.err
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=aschmid@mit.edu
 
