7ed948b45c4a46e7fea694a81d41dc51749fe438
Alex Schmid <alexandriaschmid3@gmail.com>
2024-06-17 09:18:24 -0400

    Add supercloud shell script for time disc runs

--- relay_sc.sh
+++ relay_sc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -a 585-848
+#SBATCH -a 22-41
 #SBATCH --partition=xeon-p8
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=1-04:00
-#SBATCH -o /home/gridsan/aschmid/relaylogistics/outerr/table2_%a.out
-#SBATCH -e /home/gridsan/aschmid/relaylogistics/outerr/table2_%a.err
+#SBATCH --time=2-00:00
+#SBATCH -o /home/gridsan/aschmid/relaylogistics/outerr/onlinesense_%a.out
+#SBATCH -e /home/gridsan/aschmid/relaylogistics/outerr/onlinesense_%a.err
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=aschmid@mit.edu
 
 module load julia/1.9.2
-module load gurobi/gurobi-1000
+module load gurobi/gurobi-1102
 
-julia run_static.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
+julia run_online.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
