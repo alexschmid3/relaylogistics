8a53856c2ca6bf6596290ef7be9aad6cce30486f
Alex Schmid <alexandriaschmid3@gmail.com>
2024-06-19 14:16:19 -0400

    Add batch script for supercloud timedelta runs

--- relay_sc.sh
+++ relay_sc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -a 1-16
+#SBATCH -a 22-41
 #SBATCH --partition=xeon-p8
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
@@ -12,4 +12,4 @@
 module load julia/1.9.2
 module load gurobi/gurobi-1102
 
-julia run_static.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
+julia run_online.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
