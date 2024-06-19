9e7099a22e296fb0d8a1a2b76b3d434b34144789
Alex Schmid <alexandriaschmid3@gmail.com>
2024-06-19 10:32:16 -0400

    Driver staffing runs on supercloud

--- relay_sc.sh
+++ relay_sc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -a 22-41
+#SBATCH -a 1-64
 #SBATCH --partition=xeon-p8
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --time=2-00:00
-#SBATCH -o /home/gridsan/aschmid/relaylogistics/outerr/onlinesense_%a.out
-#SBATCH -e /home/gridsan/aschmid/relaylogistics/outerr/onlinesense_%a.err
+#SBATCH -o /home/gridsan/aschmid/relaylogistics/outerr/hiring_%a.out
+#SBATCH -e /home/gridsan/aschmid/relaylogistics/outerr/hiring_%a.err
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=aschmid@mit.edu
 
 module load julia/1.9.2
 module load gurobi/gurobi-1102
 
-julia run_online.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
+julia run_static.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
